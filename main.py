ee16b5238b2c6f3a54ac399daac0573a91c0341e
melrudexqwert-cmd <melrudexqwert@gmail.com>
2026-09-23 19:08:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 
 # ================= НАСТРОЙКИ СИСТЕМЫ =================
-BOT_TOKEN = "8500361446:AAGPk_BesSt0WJDOvVqyMNykr-5jcg5BE0k"
+BOT_TOKEN = "8500361446:AAGEzSq5irgTnNrlXXCv9Y22kvXiggzTWpI"
 ADMIN_ID = 1154469594  # Твой цифровой Telegram ID (узнать в @userinfobot)
 DB_PATH = "group_study.db"
 
